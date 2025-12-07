7c22185a7cd1cb89aa987a38a51ce5f3780fb671
ctudel <christudela@u.boisestate.edu>
2025-12-07 13:38:17 -0700

    Documents service layer

--- api/src/main/java/com/challenge/api/controller/EmployeeController.java
+++ api/src/main/java/com/challenge/api/controller/EmployeeController.java
@@ -3,9 +3,11 @@ package com.challenge.api.controller;
 import com.challenge.api.model.Employee;
 import java.util.List;
 import java.util.UUID;
+
+import com.challenge.api.service.EmployeeService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
@@ -14,30 +16,39 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/api/v1/employee")
 public class EmployeeController {
+    private EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
 
     /**
      * @implNote Need not be concerned with an actual persistence layer. Generate mock Employee models as necessary.
      * @return One or more Employees.
      */
     public List<Employee> getAllEmployees() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        try { return service.getAll(); } catch (Error e) { throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR); }
     }
 
     /**
      * @implNote Need not be concerned with an actual persistence layer. Generate mock Employee model as necessary.
      * @param uuid Employee UUID
      * @return Requested Employee if exists
+     * @implSpec Expects the uuid to be passed through the path
      */
-    public Employee getEmployeeByUuid(UUID uuid) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    @GetMapping("/{uuid}")
+    public Employee getEmployeeByUuid(@PathVariable UUID uuid) {
+        try { return service.getById(uuid); } catch (Error e) { throw new ResponseStatusException(HttpStatus.NOT_FOUND); }
     }
 
     /**
      * @implNote Need not be concerned with an actual persistence layer.
-     * @param requestBody hint!
+     * @param requestBody POST request body
      * @return Newly created Employee
+     * @implSpec Validates request body has the shape of an Employee and passes it to the service layer
      */
-    public Employee createEmployee(Object requestBody) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    @PostMapping("")
+    public Employee createEmployee(@Validated(Employee.class) @RequestBody Employee requestBody) {
+        try { return service.create(requestBody); } catch (Error e) { throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR); }
     }
 }
